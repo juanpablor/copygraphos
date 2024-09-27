f53d5e29a16f06c4b57842c3970556856bee5998
Juan Pablo Riano <jp@pricerqx.com>
2024-09-26 22:35:43 -0400

    Updates

---
+++ 5d416436-da9bfc2e11ce792d0618.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcopygrafos=self.webpackChunkcopygrafos||[]).push([[321],{9042:function(r,t,n){n.d(t,{Zuw:function(){return e}});var a=n(3621);function e(r){return(0,a.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"},child:[]}]})(r)}}}]);
+//# sourceMappingURL=5d416436-da9bfc2e11ce792d0618.js.map
