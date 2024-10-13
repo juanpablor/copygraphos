4ba4b50e8bd5692023590a70a007e10f94fc9e4f
Juan Pablo Riano <jp@pricerqx.com>
2024-10-12 23:30:56 -0400

    Add banners provided by client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const IndexPage: React.FC<PageProps> = () => {
       <LayoutPage>
         <div className={styles.container}>
           <img className="hidden sm:flex max-w-[1200px] mx-auto -mt-4" src={images.banner} alt={images.banner} />
-          {/* <img className="sm:hidden flex max-w-[320px] mx-auto" src={images.bannerSq} alt={images.banner} /> */}
+          <img className="sm:hidden flex max-w-[320px] mx-auto" src={images.bannerSq} alt={images.banner} />
           <div className="flex w-full sm:w-3/4 flex-col sm:flex-row gap-4 mx-auto">
             {items.map((item, index) => (
               <div key={index} className={styles.card}>
